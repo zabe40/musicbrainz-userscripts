77da09132b6decae712a2d1d837ba83deaeb485c
Zabe <zabe@disroot.org>
2024-01-15 22:24:44 -0500

    Broaden @match header

--- bandcamp-tag-importer.user.js
+++ bandcamp-tag-importer.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         MusicBrainz Bandcamp Tag Importer
 // @namespace    https://github.com/zabe40
-// @version      2024-01-15_2
+// @version      2024-01-15_3
 // @description  Easily submit tags on Bandcamp pages to Musicbrainz
 // @author       zabe
 // @homepage     https://github.com/zabe40/musicbrainz-userscripts
 // @updateURL    https://raw.github.com/zabe40/musicbrainz-userscripts/main/bandcamp-tag-importer.user.js
 // @downloadURL  https://raw.github.com/zabe40/musicbrainz-userscripts/main/bandcamp-tag-importer.user.js
 // @supportURL   https://github.com/zabe40/musicbrainz-userscripts/issues
-// @match        https://musicbrainz.org/release/*
+// @match        http*://*.musicbrainz.org/release/*
 // @connect      bandcamp.com
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
