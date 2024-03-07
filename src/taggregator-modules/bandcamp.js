fbdc998a07a092227798aaecf946ad55a6c1cc28
Zabe <zabe@disroot.org>
2024-03-07 12:22:38 -0500

    Taggregator: Copy Bandcamp's location tag comment from bandcamp importer

--- src/taggregator-modules/bandcamp.js
+++ src/taggregator-modules/bandcamp.js
@@ -6,6 +6,12 @@ function fetchBandcampTags(url){
             let results = [];
             html.querySelectorAll("a.tag")
                 .forEach((currentAnchor, currentIndex, listObj) => {
+                    // on Bandcamp the last tag on an album is a tag
+                    // of the city in the artist's profile. this
+                    // information is often innaccurate (in the case
+                    // of labels with Bandcamp pages) or outdated, and
+                    // regardless the information is better
+                    // represented via a relationship of some sort
                     if(currentIndex != listObj.length - 1){
                         results.push(currentAnchor.innerText);
                     }
