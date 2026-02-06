4e32902c4c3e4b360143e0f2d1b1f7404954a37d
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-02-05 22:59:28 -0600

    Add support for fetching artist tags from Bandcamp
    Fetches using tags from the artist search results
    https://bandcamp.com/search?q=yonkagor&item_type=b

--- src/taggregator-modules/bandcamp.js
+++ src/taggregator-modules/bandcamp.js
@@ -1,6 +1,35 @@
 import { fetchAsHTML} from '../fetch.js';
 
 function fetchBandcampTags(url, entityType){
+    if (entityType == "artist") {
+        const match = url.match(/:\/\/([^.]*).bandcamp.com/)
+        if (!match || match?.length != 2) return (() => [])
+        const bcSearchURL = `https://bandcamp.com/search?q=${match[1]}&item_type=b`
+        return fetchAsHTML(bcSearchURL)
+          .then((html) => {
+              let results = [];
+              html.querySelectorAll("div.itemurl")
+                  .forEach((currentUrl, currentIndex, listObj) => {
+                      // Bandcamp provides artist tags to artist search results
+                      if (currentUrl.innerHTML.includes(`${match[1]}.bandcamp.com`)){
+                        let sibling = currentUrl.nextElementSibling;
+                        while (sibling) {
+                          if (sibling.className == "genre"){
+                            results.push(sibling.innerText.replace("genre:", "").trim())
+                          }
+                          if (sibling.className == "tags data-search"){
+                            const tags = sibling.innerText.replace("tags:", "").split(",")
+                            tags.forEach((tag) => {
+                              results.push(tag.trim());
+                            })
+                          }
+                          sibling = sibling.nextElementSibling;
+                        }
+                      }
+                  });
+              return results;
+          });
+    }
     return fetchAsHTML(url)
         .then((html) => {
             let results = [];
@@ -25,6 +54,6 @@ function fetchBandcampTags(url, entityType){
 
 export const bandcamp = { domain: "bandcamp.com",
                           fetchTags: fetchBandcampTags,
-                          supportedTypes: ["release","recording"],
+                          supportedTypes: ["release","recording","artist"],
                           name: "Bandcamp",
                           faviconClass: "bandcamp-favicon",};
